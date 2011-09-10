4e422a99c59791e76a1060f46f66a3c607673d54
Juanchisan <saeztandil@gmail.com>
2011-09-10 20:12:16 -0300

    Nose

---
+++ src-pos/com/openbravo/pos/payment/MyPaymentGateway.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.openbravo.pos.payment;
+
+/**
+ *
+ * @author Juanchisan
+ */
+public class MyPaymentGateway implements PaymentGateway {
+
+    @Override
+    public void execute(PaymentInfoMagcard payinfo) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    /*
+     * 
+     */
+}
